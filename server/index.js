177813bc305fd7fb14924d3b5aac60cbde675e94
bosariemen755-create <bosariemen755@gmail.com>
2026-06-13 07:53:42 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,3 +12,34 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
   console.log("Server running");
 });
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const users = require("./users");
+
+// SIGN UP
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  users.push({ email, password: hashedPassword });
+
+  res.json({ message: "User created" });
+});
+
+// LOGIN
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find(u => u.email === email);
+
+  if (!user) return res.status(400).json({ error: "User not found" });
+
+  const valid = await bcrypt.compare(password, user.password);
+
+  if (!valid) return res.status(400).json({ error: "Invalid password" });
+
+  const token = jwt.sign({ email }, "secret123");
+
+  res.json({ token });
+});
